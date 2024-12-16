e27e84d8406fe753134403aeafdb65e9669dde4c
Andrey Tikhonov <mastertixon.is@gmail.com>
2024-12-16 10:09:43 +0500

    init

--- core/celery/services.py
+++ core/celery/services.py
@@ -44,6 +44,7 @@ async def send_message_to_channel():
 
     try:
         posts = db.get_posts_to_send()
+        logger.debug(posts)
         if not posts:
             logger.info("Нет постов для отправки")
             return {"Status": "Нет постов для отправки"}
